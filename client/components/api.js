fc46fcb66624b1171ed98f94ece7f6659724fc04
Kyle Watson <67244779+kaydub26@users.noreply.github.com>
2020-09-04 10:41:33 +1200

    " api.js file and fn created - change .get link"

--- client/components/api.js
+++ client/components/api.js
@@ -1,7 +1,5 @@
 import request from 'superagent'
 
-const serverURL = 'http://localhost:3000/api/v1'
-
 export function getJoke () {
   return request
     .get('https://official-joke-api.appspot.com/random_joke')
