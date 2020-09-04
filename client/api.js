5f543e4a34dfe337b344ee1b195a53465014bb3d
Kyle Watson <67244779+kaydub26@users.noreply.github.com>
2020-09-04 14:09:38 +1200

    add working city locator

--- client/api.js
+++ client/api.js
@@ -1,19 +1,22 @@
 import request from 'superagent'
 
-
-export function getWeather () {
-  let city = ''
-   function getCity (input) {
-     city = input
-    capitalFirst(city)
-  }
+let city = ''
+export function getWeather (finalCity) {
+ 
+  
   return request
     .get('https://api.openweathermap.org/data/2.5/weather')
-    .query({q: city})
+    .query({q: finalCity})
     .query({appid:'981cdb40f64b3db85d7ff026a5d84c86'})
     .then(response => response.body)
 }
 
+
+function getCity (input) {
+  city = input
+ capitalFirst(city)
+}
 function capitalFirst (input) {
   return input.charAt(0).toUpperCase() + input.slice(1) 
 }
+
--- client/components/WeatherAPI.jsx
+++ client/components/WeatherAPI.jsx
@@ -5,26 +5,66 @@ class WeatherApi extends React.Component {
   state = {
     weather: [],
     main: null,
-    wind: null
+    wind: null,
+    input: ''
   }
 
-  componentDidMount () {
-    getWeather()
-      .then(wth => { console.log(wth.weather)
-        this.setState({
-          weather: wth.weather,
-          main: wth.main,
-          wind: wth.wind
-        })
-      })
+  // componentDidMount () {
+  //   getWeather(this.state.input)
+  //     .then(wth => { console.log(wth.weather)
+  //       this.setState({
+  //         weather: wth.weather,
+  //         main: wth.main,
+  //         wind: wth.wind
+  //       })
+  //     })
+  // }
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
   }
+
+
+
+   capitalFirst = (input) => {
+    return input.charAt(0).toUpperCase() + input.slice(1) 
+  }
+
+
+
+
+handleClick = () => {
+  console.log(this.state.input)
+  
+  const input = this.state.input
+ 
+ let finalCity = this.capitalFirst(input) 
+ console.log(finalCity)
+  getWeather(finalCity)
+  .then(wth => { console.log(wth.weather)
+          this.setState({
+            weather: wth.weather,
+            main: wth.main,
+            wind: wth.wind,
+            input: ''
+          })
+        })
+    }
+  
+
+    
+    
 
   render () {
     const {weather, main, wind} = this.state
     return (
       <>
-      Search: <input 
-      type="text"/>
+      Search: <input name= 'input' 
+      value={this.state.input}
+      onChange={this.handleChange}/>
+      <button onClick={this.handleClick}>Show me the Weather</button>
       <p>
         {weather[0]?.description}
       </p>
